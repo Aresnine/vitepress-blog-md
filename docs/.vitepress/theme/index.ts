3b90b6ba5b8f4202dad53a691d886b5cecf9fef5
Aresnine <763555000@qq.com>
2023-02-25 01:31:38 +0800

    test: 第八次测试

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,13 +4,13 @@ import "vitepress-theme-demoblock/dist/theme/styles/index.css"
 // import { useComponents } from "./useComponents"
 import Demo from "vitepress-theme-demoblock/dist/client/components/Demo.vue"
 import DemoBlock from "vitepress-theme-demoblock/dist/client/components/DemoBlock.vue"
-import TableDemo from "./TableDemo.vue"
+// import TableDemo from "./TableDemo"
 import "uno.css"
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.use(XXOOUI)
-    app.component("TableDemo", TableDemo)
+    // app.component("TableDemo", TableDemo)
     app.component("Demo", Demo)
     app.component("DemoBlock", DemoBlock)
   },
